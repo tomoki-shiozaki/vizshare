1fffffa2ca7ed0530f13c558784df2ca67db0db3
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-30 10:03:42 +0900

    feat(dataset-point): add url for DatasetDataPointViewSet

--- backend/apps/api/dataset/urls.py
+++ backend/apps/api/dataset/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from apps.api.dataset.views import (
+    DatasetDataPointViewSet,
     DatasetDetailAPIView,
     DatasetListAPIView,
     DatasetUploadAPIView,
@@ -8,8 +10,17 @@ from apps.api.dataset.views import (
 
 app_name = "dataset"
 
+router = DefaultRouter()
+router.register(
+    r"(?P<dataset_id>\d+)/data-points",
+    DatasetDataPointViewSet,
+    basename="dataset-data-points",
+)
+
 urlpatterns = [
     path("upload/", DatasetUploadAPIView.as_view(), name="upload"),
     path("list/", DatasetListAPIView.as_view(), name="list"),
     path("<int:pk>/", DatasetDetailAPIView.as_view(), name="detail"),
+    # ViewSet 側
+    path("", include(router.urls)),
 ]
