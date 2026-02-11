79782c338263d7ca3bf78e72b123f68067b9b243
shiozaki <shiozaki.tomoki2@gmail.com>
2026-02-11 11:48:40 +0900

    feat(datapoint): add DatasetDataAPIView

--- backend/apps/api/dataset/urls.py
+++ backend/apps/api/dataset/urls.py
@@ -1,7 +1,7 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
-from apps.api.dataset.views import (
+from apps.api.dataset.views.datapoint import DatasetDataAPIView
+from apps.api.dataset.views.dataset import (
     DatasetDetailAPIView,
     DatasetListAPIView,
     DatasetUploadAPIView,
@@ -9,9 +9,9 @@ from apps.api.dataset.views import (
 
 app_name = "dataset"
 
-
 urlpatterns = [
     path("upload/", DatasetUploadAPIView.as_view(), name="upload"),
     path("list/", DatasetListAPIView.as_view(), name="list"),
     path("<int:pk>/", DatasetDetailAPIView.as_view(), name="detail"),
+    path("<int:pk>/data/", DatasetDataAPIView.as_view(), name="data"),
 ]
