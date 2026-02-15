70bb4e222aa8dd6300fa118b86820bd8333501dd
shiozaki <shiozaki.tomoki2@gmail.com>
2026-02-15 12:32:09 +0900

    chore(infra): refactor infra/ directory

--- infra/main.tf
+++ infra/main.tf
@@ -9,3 +9,10 @@ module "cloudbuild" {
 
   cloudbuild_service_account = google_service_account.cloudbuild_runner.id
 }
+
+module "iam" {
+  source = "./iam"
+
+  project_id     = var.project_id
+  project_number = var.project_number
+}
