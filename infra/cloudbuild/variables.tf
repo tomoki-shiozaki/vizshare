25d1eb9d3b03b13b82a18dafd595f52dfe14ac25
shiozaki <shiozaki.tomoki2@gmail.com>
2026-02-15 11:18:39 +0900

    chore(infra): refactor infra/ directory

---
+++ infra/cloudbuild/variables.tf
@@ -0,0 +1,5 @@
+variable "project_id" {}
+variable "service_name" {}
+variable "region" {}
+variable "github_repo" {}
+variable "github_owner" {}
---
+++ infra/main.tf
@@ -0,0 +1,9 @@
+module "cloudbuild" {
+  source = "./cloudbuild"
+
+  project_id   = var.project_id
+  service_name = var.service_name
+  region       = var.region
+  github_repo  = var.github_repo
+  github_owner = var.github_owner
+}
