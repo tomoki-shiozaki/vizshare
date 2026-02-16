00e4a5772947b0197cad2fe76b64a26c4767bc88
tomoki-shiozaki <shiozaki.tomoki2@gmail.com>
2026-02-16 18:23:57 +0900

    Fix/deploy (#15)
    * fix(deploy): fix celery settings

--- backend/apps/dataset/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-
-from apps.dataset.services.tasks_runner import parse_dataset_sync
-
-
-@shared_task
-def parse_dataset_task(dataset_id: int):
-    # Celery タスクから同期関数を呼ぶだけ
-    parse_dataset_sync(dataset_id)
