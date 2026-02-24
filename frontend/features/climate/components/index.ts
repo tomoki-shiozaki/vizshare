413498ef7f829e086b188bd00629cf390c9580b2
shiozaki <shiozaki.tomoki2@gmail.com>
2026-02-24 18:43:29 +0900

    chore: remove climate app

--- frontend/features/climate/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { TemperatureChart } from "./TemperatureChart";
-export { TemperatureChartExplanation } from "./TemperatureChartExplanation";
-export { TemperatureDataSource } from "./TemperatureDataSource";
-
-export { CO2WorldMap } from "./CO2WorldMap";
-export { CO2MapExplanation } from "./CO2MapExplanation";
-export { CO2DataSource } from "./CO2DataSource";
