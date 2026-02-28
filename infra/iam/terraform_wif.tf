d733a7dbee3fb03359fe7a7f706a713b5b77c199
shiozaki <shiozaki.tomoki2@gmail.com>
2026-02-28 12:20:31 +0900

    chore(terraform): add more permissions to SA for WIF (#40)

--- infra/iam/terraform_wif.tf
+++ infra/iam/terraform_wif.tf
@@ -20,7 +20,7 @@ resource "google_project_iam_member" "terraform_sa_viewer" {
 resource "google_service_account_iam_member" "terraform_wif_binding" {
   service_account_id = google_service_account.terraform_sa.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.repository_owner/tomoki-shiozaki"
+  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.repository/tomoki-shiozaki/vizshare"
 }
 
 # GitHub Actions が terraform-sa の access token を発行できるようにする
@@ -28,7 +28,7 @@ resource "google_service_account_iam_member" "terraform_wif_token_creator" {
   service_account_id = google_service_account.terraform_sa.name
   role               = "roles/iam.serviceAccountTokenCreator"
 
-  member = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.repository_owner/tomoki-shiozaki"
+  member = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.repository/tomoki-shiozaki/vizshare"
 }
 
 resource "google_storage_bucket_iam_member" "terraform_state_access" {
