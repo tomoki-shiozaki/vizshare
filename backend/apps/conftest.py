cb174fad274bc0a81a371f0efb254476503a92c6
shiozaki <shiozaki.tomoki2@gmail.com>
2026-03-07 17:54:52 +0900

    test(api): add tests for create_dataset function

---
+++ backend/apps/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+
+User = get_user_model()
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(username="testuser", password="pass")
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
