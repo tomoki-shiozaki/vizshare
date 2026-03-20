00d5e2be7616eb2fb6bc2ce9f28b7b790a471367
shiozaki <shiozaki.tomoki2@gmail.com>
2026-03-20 17:21:01 +0900

    feat(datasets): add url path for PublicDatasetDetailAPIView

--- backend/apps/api/dataset/urls.py
+++ backend/apps/api/dataset/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from apps.api.dataset.views.dataset_read import (
     DatasetDetailAPIView,
     DatasetListAPIView,
+    PublicDatasetDetailAPIView,
     PublicDatasetListAPIView,
 )
 from apps.api.dataset.views.dataset_timeseries import DatasetTimeSeriesAPIView
@@ -16,4 +17,7 @@ urlpatterns = [
     path("<int:pk>/", DatasetDetailAPIView.as_view(), name="detail"),
     path("<int:pk>/timeseries/", DatasetTimeSeriesAPIView.as_view(), name="timeseries"),
     path("public/", PublicDatasetListAPIView.as_view(), name="public"),
+    path(
+        "public/<int:pk>/", PublicDatasetDetailAPIView.as_view(), name="public-detail"
+    ),
 ]
