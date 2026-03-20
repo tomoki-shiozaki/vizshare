f14a498b9ab9fc49037f2af364bfec0be3dc09af
shiozaki <shiozaki.tomoki2@gmail.com>
2026-03-20 17:25:12 +0900

    feat(datasets): add api client for public dataset detail component

--- frontend/features/datasets/types/publicDataset.ts
+++ frontend/features/datasets/types/publicDataset.ts
@@ -1,4 +1,15 @@
 import type { paths } from "@/types/api";
 
+// =============================
+// List
+// =============================
+
 export type PaginatedPublicDatasetListResponse =
   paths["/api/v1/datasets/public/"]["get"]["responses"][200]["content"]["application/json"];
+
+// =============================
+// Detail
+// =============================
+
+export type PublicDatasetDetailResponse =
+  paths["/api/v1/datasets/public/{id}/"]["get"]["responses"][200]["content"]["application/json"];
