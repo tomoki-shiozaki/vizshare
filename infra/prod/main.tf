6f809e4176aa4ffac2f2d9d49bea55aa332bc97e
shiozaki <shiozaki.tomoki2@gmail.com>
2026-03-24 16:57:16 +0900

    chore(infra): refactor infra directory

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -1,18 +1,15 @@
 module "cloudbuild" {
-  source = "./cloudbuild"
-
+  source = "../modules/cloudbuild"   
   project_id   = var.project_id
   service_name = var.service_name
   region       = var.region
   github_repo  = var.github_repo
   github_owner = var.github_owner
-
   cloudbuild_service_account = module.iam.cloudbuild_runner_sa_id
 }
 
 module "iam" {
-  source = "./iam"
-
+  source = "../modules/iam"
   project_id     = var.project_id
   project_number = var.project_number
   media_bucket_name = module.storage.media_bucket_name
@@ -20,8 +17,7 @@ module "iam" {
 }
 
 module "storage" {
-  source = "./storage"
-
+  source = "../modules/storage"
   project_id   = var.project_id
   service_name = var.service_name
 }
