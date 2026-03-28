e3b56a1f8f10b444b740f560e91b5f242b9812db
shiozaki <shiozaki.tomoki2@gmail.com>
2026-03-28 17:13:42 +0900

    chore(infra): update prod resources

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -4,10 +4,16 @@ module "iam" {
   project_number = var.project_number
   media_bucket_name = module.storage.media_bucket_name
   terraform_state_bucket_name = var.terraform_state_bucket_name
+
+  service_name = var.service_name
+  env          = var.env
+  github_owner = var.github_owner
+  github_repo  = var.github_repo
 }
 
 module "storage" {
   source = "../modules/storage"
   project_id   = var.project_id
   service_name = var.service_name
+  env          = var.env
 }
