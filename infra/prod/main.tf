04430333582f31f8bf7aa7017bf46dd9ef5580eb
shiozaki <shiozaki.tomoki2@gmail.com>
2026-03-28 12:57:34 +0900

    chore(infra): update prod resources

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -6,6 +6,7 @@ module "cloudbuild" {
   github_repo  = var.github_repo
   github_owner = var.github_owner
   cloudbuild_service_account = module.iam.cloudbuild_runner_sa_id
+  env = var.env   
 }
 
 module "iam" {
@@ -14,10 +15,12 @@ module "iam" {
   project_number = var.project_number
   media_bucket_name = module.storage.media_bucket_name
   terraform_state_bucket_name = var.terraform_state_bucket_name
+  env = var.env   
 }
 
 module "storage" {
   source = "../modules/storage"
   project_id   = var.project_id
   service_name = var.service_name
+  env = var.env   
 }
