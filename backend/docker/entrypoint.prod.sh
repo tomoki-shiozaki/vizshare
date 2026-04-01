7da793bef797daf3f5d747844a7ff0875269f30e
shiozaki <shiozaki.tomoki2@gmail.com>
2026-04-01 16:47:33 +0900

    chore(settings): improve entrypoint.prod.sh

--- backend/docker/entrypoint.prod.sh
+++ backend/docker/entrypoint.prod.sh
@@ -5,7 +5,6 @@ set -ex  # x で実行コマンドを表示、e でエラー時に停止
 export DJANGO_SETTINGS_MODULE=config.settings
 
 echo "Running migrations..."
-# エラーが出ても無理に停止させたくない場合は || true を追加も可能
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
@@ -14,6 +13,9 @@ python manage.py collectstatic --noinput
 echo "Starting Gunicorn on port $PORT..."
 # 0.0.0.0 でバインド、ワーカー数は必要に応じて調整
 exec gunicorn config.wsgi:application \
-    --bind 0.0.0.0:$PORT \
-    --workers 3 \
-    --timeout 120  # 必要に応じてタイムアウト延長
+  --bind 0.0.0.0:$PORT \
+  --workers 2 \
+  --threads 4 \
+  --timeout 120 \
+  --access-logfile - \
+  --error-logfile -
