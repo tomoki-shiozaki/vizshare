a33be993cf159b18c32896bbb1cda2b9f247edf6
shiozaki <shiozaki.tomoki2@gmail.com>
2026-04-11 17:12:08 +0900

    chore(infra): execute migration by cloud run job

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -1,8 +1,17 @@
+module "storage" {
+  source = "../modules/storage"
+
+  project_id   = var.project_id
+  service_name = var.service_name
+  env          = var.env
+}
+
 module "iam" {
   source = "../modules/iam"
-  project_id     = var.project_id
-  project_number = var.project_number
-  media_bucket_name = module.storage.media_bucket_name
+
+  project_id                  = var.project_id
+  project_number              = var.project_number
+  media_bucket_name           = module.storage.media_bucket_name
   terraform_state_bucket_name = var.terraform_state_bucket_name
 
   service_name = var.service_name
@@ -11,19 +20,13 @@ module "iam" {
   github_repo  = var.github_repo
 }
 
-module "storage" {
-  source = "../modules/storage"
-  project_id   = var.project_id
-  service_name = var.service_name
-  env          = var.env
-}
-
 module "cloudrun" {
   source = "../modules/cloudrun"
 
+  project_id  = var.project_id
   service_name = var.service_name
-  job_name     = "vizshare-staging-migrate"
-  region       = "asia-northeast1"
-  image        = "docker.io/shiozaki1/vizshare:staging"
   env          = var.env
+  region       = var.region
+  job_name = "${var.service_name}-${var.env}-migrate"
+  image    = "docker.io/shiozaki1/${var.service_name}:${var.env}"
 }
