fc71c824fc37bdf2f4413b65c1f507a85e207e03
shiozaki <shiozaki.tomoki2@gmail.com>
2026-04-30 15:12:05 +0900

    feat(throttling): add throttling setting

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,4 +1,5 @@
 from dj_rest_auth.views import LoginView
+from dj_rest_auth.registration.views import RegisterView
 from rest_framework.throttling import AnonRateThrottle
 
 
@@ -6,5 +7,13 @@ class LoginThrottle(AnonRateThrottle):
     rate = "5/min"
 
 
+class RegisterThrottle(AnonRateThrottle):
+    rate = "5/min"
+
+
 class CustomLoginView(LoginView):
     throttle_classes = [LoginThrottle]
+
+
+class CustomRegisterView(RegisterView):
+    throttle_classes = [RegisterThrottle]
