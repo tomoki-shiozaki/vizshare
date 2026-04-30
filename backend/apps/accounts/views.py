3c797bfc5c3fbd48b019f06949f1b855f4d0607c
shiozaki <shiozaki.tomoki2@gmail.com>
2026-04-30 15:26:32 +0900

    feat(throttling): add throttling setting

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,6 +1,8 @@
 from dj_rest_auth.views import LoginView
 from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.serializers import JWTSerializer
 from rest_framework.throttling import AnonRateThrottle
+from drf_spectacular.utils import extend_schema
 
 
 class LoginThrottle(AnonRateThrottle):
@@ -14,6 +16,14 @@ class RegisterThrottle(AnonRateThrottle):
 class CustomLoginView(LoginView):
     throttle_classes = [LoginThrottle]
 
+    @extend_schema(responses=JWTSerializer)
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 class CustomRegisterView(RegisterView):
     throttle_classes = [RegisterThrottle]
+
+    @extend_schema(responses=JWTSerializer)
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
