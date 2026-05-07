972ad9afed8da2a45872c1e82e440dd7c476785c
shiozaki <shiozaki.tomoki2@gmail.com>
2026-05-07 16:51:21 +0900

    feat(dataset): add anonymous dataset support

--- backend/apps/api/dataset/tests/views/test_dataset_write.py
+++ backend/apps/api/dataset/tests/views/test_dataset_write.py
@@ -80,14 +80,16 @@ class TestDatasetVisibilityUpdateAPIView:
             name="My Dataset",
             owner=user,
             schema={"time": "time_col", "metrics": ["value"]},
-            is_public=False,
+            visibility=Dataset.Visibility.PRIVATE,
         )
         url = reverse("dataset:visibility", args=[dataset.pk])
 
-        response = api_client.patch(url, {"is_public": True}, format="json")
+        response = api_client.patch(
+            url, {"visibility": Dataset.Visibility.PUBLIC}, format="json"
+        )
         assert response.status_code == status.HTTP_200_OK  # type: ignore
         dataset.refresh_from_db()
-        assert dataset.is_public is True
+        assert dataset.visibility == Dataset.Visibility.PUBLIC
 
     def test_visibility_update_permission_denied(
         self, user, another_user, api_client: APIClient
@@ -97,12 +99,14 @@ class TestDatasetVisibilityUpdateAPIView:
             name="Other Dataset",
             owner=another_user,
             schema={"time": "time_col", "metrics": ["value"]},
-            is_public=False,
+            visibility=Dataset.Visibility.PRIVATE,
         )
         api_client.force_authenticate(user=user)
         url = reverse("dataset:visibility", args=[other_user_dataset.pk])
 
-        response = api_client.patch(url, {"is_public": True}, format="json")
+        response = api_client.patch(
+            url, {"visibility": Dataset.Visibility.PUBLIC}, format="json"
+        )
         assert (
             response.status_code == status.HTTP_404_NOT_FOUND  # type: ignore
         )  # queryset に入らないため
