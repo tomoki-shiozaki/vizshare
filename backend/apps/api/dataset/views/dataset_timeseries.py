02861ba8b89028f2334b31b6aacb8bdd3b3d0363
shiozaki <shiozaki.tomoki2@gmail.com>
2026-05-18 16:52:25 +0900

    feat(datset): add AnonymousDatasetTimeSeriesAPIView

--- backend/apps/api/dataset/views/dataset_timeseries.py
+++ backend/apps/api/dataset/views/dataset_timeseries.py
@@ -1,9 +1,8 @@
 from uuid import UUID
 
 from django.shortcuts import get_object_or_404
-from django.utils import timezone
 from rest_framework import status
-from rest_framework.exceptions import NotFound, PermissionDenied
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
